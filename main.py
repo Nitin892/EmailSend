e3de5099912c0b13651a45e21d632d6c91bf4f7d
Nitin892 <nitinshenigaram7860@gmail.com>
2022-02-05 11:56:32 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import smtplib
+server=smtplib.SMTP('smtp.gmail.com',587)
+server.starttls()
+server.login('sender email','Sender password')
+server.sendmail('sender email','receiver email','type your message')
+
+
+# make sure must turn on Less secure app access
+# steps to turn on:
+
+#1) got to Security
+#2) make sure turn off 2_step verification
+#3) turn on Less secure app access
