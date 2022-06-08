412d0c8bbc47a67688ffd4c6529fae5a8d637c4b
Nitin892 <78948806+Nitin892@users.noreply.github.com>
2022-06-08 19:28:27 +0530

    Update main.py
    Changed

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ server.sendmail('sender email','receiver email','type your message')
 # make sure must turn on Less secure app access
 # steps to turn on:
 
-#1) got to Security
+#1) go to Security
 #2) make sure turn off 2_step verification
 #3) turn on Less secure app access
